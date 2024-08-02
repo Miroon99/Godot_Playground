bfcce321f1de3dc5c11a30890fb6fd52ed96b06c
mohamadalimran <mohamadalimran@airasia.com>
2024-08-03 01:17:24 +0800

    combine arif-imran feature

--- Scripts/Game_Area.gd
+++ Scripts/Game_Area.gd
@@ -5,14 +5,56 @@ var player = player_obj.instantiate()
 
 @onready var cannon = $Cannon
 
+var gameRunning: bool
+var gameOver: bool
+var scroll = 0
+var score = 0
+var SCROLL_SPEED: int = 3
+var screenSize: Vector2i
+var groundHeight: int
+var pipes: Array
+const PIPE_DELAY: int = 100
+const PIPE_RANGE: int = 200
+const X_AXIS_BIRD_FLYING = 400
+
 func _ready():
-	pass # Replace with function body.
+	screenSize = get_window().size
+	#newGame()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if gameRunning:
+		scroll += SCROLL_SPEED
+		if scroll >= screenSize.x:
+			scroll = 0
+		$Ground.position.x = -scroll
+		
+		
 	if Input.is_action_just_pressed("cannon_fire"):
 		player.position = cannon.getPlayerSpawnPosition()
 		player.rotation = cannon.getBodyRotation()
 		add_child(player)
 		move_child(player, 0)
 		player.flyFromCannonFire(1000)
+
+#func newGame():
+	#gameRunning = false
+	#gameOver = false
+	#score = 0
+	#scroll = 0
+	#$Bird.resetBird()
+	
+func moveGround():
+	gameRunning = true
+	
+func stopGround():
+	gameRunning = false
+
+func _physics_process(delta):
+	if player.position.x >= X_AXIS_BIRD_FLYING:
+		player.velocity.x = 0
+		moveGround()
+	
+	if player.is_on_floor():
+		stopGround()
+
